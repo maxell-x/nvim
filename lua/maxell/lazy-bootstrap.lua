c002459cb672420b71b90ecc2b35418bf9ffb90d
maxell-x <maxell.info@icloud.com>
2024-02-29 13:13:09 +0100

    added telescope

--- lua/maxell/lazy-bootstrap.lua
+++ lua/maxell/lazy-bootstrap.lua
@@ -11,4 +11,12 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("maxell.plugins")
+require("lazy").setup("maxell.plugins", {
+    checker = {
+        enabled = true,
+        notify = false,
+    },
+    change_detection = {
+        notify = false,
+    },
+})
