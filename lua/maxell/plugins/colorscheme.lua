576555d9a5baff2d31c7996a340ae58a7ee71c22
maxell-x <maxell.info@icloud.com>
2024-02-29 13:12:14 +0100

    Tokyo Night Day

--- lua/maxell/plugins/colorscheme.lua
+++ lua/maxell/plugins/colorscheme.lua
@@ -1,9 +1,8 @@
 return {
-"Th3Whit3Wolf/one-nvim",
+"folke/tokyonight.nvim",
 priority = 1000,
 lazy = false,
 config = function()
-	vim.cmd("colorscheme one-nvim")
-    vim.cmd("set background=dark")
+    vim.cmd[[colorscheme tokyonight-day]]
 end,
 }
