0f06e0a99de0ca2ee6f879bd195bb72787e11d4e
maxell-x <maxell.info@icloud.com>
2024-03-27 17:42:06 +0100

    mason added

--- lua/maxell/lazy-bootstrap.lua
+++ lua/maxell/lazy-bootstrap.lua
@@ -11,7 +11,7 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("maxell.plugins", {
+require("lazy").setup({{import = "maxell.plugins"}, {import = "maxell.plugins.lsp"}}, {
     checker = {
         enabled = true,
         notify = false,
